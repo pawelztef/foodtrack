c6303c98fbad687ec03d30e05cd3e458bdfbd9f2
pawelztef <pawelztef@gmail.com>
2017-06-21 05:09:20 +0100

    quickfix in app cont after sing up method changed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,11 +4,7 @@ class ApplicationController < ActionController::Base
   layout :layout_by_resource
 
   def after_sign_in_path_for(resource)
-    if current_user
-      user_path(current_user)
-    elsif current_admin
       backend_dashboards_path
-    end
   end
 
   protected
