8d92114d86adced550fd843c41aa493c83edce11
pawelztef <pawelztef@gmail.com>
2017-06-25 09:31:49 +0100

    maps integration

--- app/models/stop.rb
+++ app/models/stop.rb
@@ -1,8 +1,10 @@
 class Stop < ActiveRecord::Base
   belongs_to :track
   before_save :set_active
-
   validates :address_line1, :address_line2, :town, :code, :date, presence: true
+
+  geocoded_by :full_street_address
+  after_validation :geocode
 
   def full_street_address
     return "#{self.address_line1} #{self.address_line2} #{self.town} #{self.code}"
