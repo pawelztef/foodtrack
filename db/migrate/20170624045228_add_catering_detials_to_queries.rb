51c0b71980400a9d7f6fbd1b842b88468e09848b
pawelztef <pawelztef@gmail.com>
2017-08-15 07:41:14 +0100

    reset of frontend

--- db/migrate/20170624045228_add_catering_detials_to_queries.rb
+++ db/migrate/20170624045228_add_catering_detials_to_queries.rb
@@ -1,7 +1,7 @@
 class AddCateringDetialsToQueries < ActiveRecord::Migration
   def change
-    add_column :queries, :from_date, :date
-    add_column :queries, :to_date, :date
+    add_column :queries, :from_date, :datetime
+    add_column :queries, :to_date, :datetime
     add_column :queries, :town, :string
     add_column :queries, :catering_flag, :boolean, defaul: false
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,19 +1,19 @@
-a = Admin.new
-a.first_name = "Jan"
-a.last_name = "Jankowksi"
-a.email = "food@food.com"
-a.password = "password"
-a.password_confirmation = "password"
-a.phone = "78878778"
-a.address_line1 = "12/2"
-a.address_line2 = "Szkolna"
-a.town = "Bobolice"
-a.code = "88-888"
-a.owner = true
-a.skip_confirmation!
-a.save!
+# a = Admin.new
+# a.first_name = "Jan"
+# a.last_name = "Jankowksi"
+# a.email = "food@food.com"
+# a.password = "password"
+# a.password_confirmation = "password"
+# a.phone = "78878778"
+# a.address_line1 = "12/2"
+# a.address_line2 = "Szkolna"
+# a.town = "Bobolice"
+# a.code = "88-888"
+# a.owner = true
+# a.skip_confirmation!
+# a.save!
 
-Setting.first_or_create(id: 1)
+# Setting.first_or_create(id: 1)
 
 track1 = Track.create( route_name: 'Trasa nr 1',
                       truck: 'Truck 1',
@@ -58,43 +58,43 @@ track1.stops << stop3
 track1.stops << stop4
 track1.save!
 
-# track2 = Track.create( route_name: 'Trasa nr 2',
-#                       truck: 'Truck 2',
-#                       driver: Faker::Name.first_name,
-#                       description: Faker::Lorem.sentence(3))
+track2 = Track.create( route_name: 'Trasa nr 2',
+                      truck: 'Truck 2',
+                      driver: Faker::Name.first_name,
+                      description: Faker::Lorem.sentence(3))
 
-# track3 = Track.create( route_name: 'Trasa nr 3',
-#                       truck: 'Truck 3',
-#                       driver: Faker::Name.first_name,
-#                       description: Faker::Lorem.sentence(3))
-# 10.times do |n| 
-#   track = Track.create( route_name: Faker::Pokemon.location,
-#                        truck: Faker::Vehicle.manufacture,
-#                        driver: Faker::Name.first_name,
-#                        description: Faker::Lorem.sentence(3))
-#   5.times do |n|
-#     stop = Stop.create(address_line1: Faker::Address.secondary_address,
-#                        address_line2: Faker::Address.street_name,
-#                        town: Faker::Address.city,
-#                        code: Faker::Address.postcode,
-#                        active: false,
-#                        date: '10/10/2010',
-#                        date_out: '11/10/2010')
-#     track.stops << stop
-#   end
-#   track.save!
-# end
-
-
-20.times do |n|
-  name = Faker::Book.unique.title
-  post = Post.new( title: name,
-                  slug: name,
-                  draft: true,
-                  publish_date: Faker::Date.between(3.years.ago, Date.today),
-                  body: Faker::Lorem.paragraph)
-  post.save!
+track3 = Track.create( route_name: 'Trasa nr 3',
+                      truck: 'Truck 3',
+                      driver: Faker::Name.first_name,
+                      description: Faker::Lorem.sentence(3))
+10.times do |n| 
+  track = Track.create( route_name: Faker::Pokemon.location,
+                       truck: Faker::Vehicle.manufacture,
+                       driver: Faker::Name.first_name,
+                       description: Faker::Lorem.sentence(3))
+  5.times do |n|
+    stop = Stop.create(address_line1: Faker::Address.secondary_address,
+                       address_line2: Faker::Address.street_name,
+                       town: Faker::Address.city,
+                       code: Faker::Address.postcode,
+                       active: false,
+                       date: '10/10/2010',
+                       date_out: '11/10/2010')
+    track.stops << stop
+  end
+  track.save!
 end
+
+
+# 20.times do |n|
+#   name = Faker::Book.unique.title
+#   post = Post.new( title: name,
+#                   slug: name,
+#                   draft: true,
+#                   publish_date: Faker::Date.between(3.years.ago, Date.today),
+#                   body: Faker::Lorem.paragraph)
+#   post.save!
+# end
 
 
 # 20.times do |n|
@@ -103,23 +103,18 @@ end
 #   post.save!
 # end
 
-10.times do |n|
-  post = Post.find(n+1)
-  post.draft = false 
-  post.save!
-end
 
-6.times do |n|
-  product = Product.new(name: "Product #{n+1}",
-                        slug: "Product #{n+1}",
-                        price: "#{n+1}0.20",
-                        image: Rails.root.join("app/assets/images/mock#{n+1}.jpg").open,
-                        description: Faker::Lorem.paragraph,
-                        catchphrase: Faker::Lorem.paragraph,
-                        publish: true,
-                        expose: true)
-  product.save
+# 6.times do |n|
+#   product = Product.new(name: "Product #{n+1}",
+#                         slug: "Product #{n+1}",
+#                         price: "#{n+1}0.20",
+#                         image: Rails.root.join("app/assets/images/mock#{n+1}.jpg").open,
+#                         description: Faker::Lorem.paragraph,
+#                         catchphrase: Faker::Lorem.paragraph,
+#                         publish: true,
+#                         expose: true)
+#   product.save
 
-end
+# end
 
 
