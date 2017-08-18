cd2769de67abfde4f451c5de85f09de5443b404f
pawelztef <pawelztef@gmail.com>
2017-08-18 12:27:30 +0100

    devise setup for invitations

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -13,7 +13,7 @@ Devise.setup do |config|
   # The period the generated invitation token is valid, after
   # this period, the invited resource won't be able to accept the invitation.
   # When invite_for is 0 (the default), the invitation won't expire.
-  config.invite_for = 1.hour 
+  config.invite_for = 3.days
   #
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
