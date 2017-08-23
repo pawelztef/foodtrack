36f2d61e7ab7143725088e52c4f2ccce78430ba2
pawelztef <pawelztef@gmail.com>
2017-08-23 06:54:01 +0100

    post model updated to handle image upload

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,9 +2,7 @@ class Post < ActiveRecord::Base
   validates :title, :publish_date, presence: true
   validates :slug, uniqueness: true
   before_save :update_slug
-  # mount_uploader :image, PostImageUploader 
-  has_one :image, as: :imageable
-
+  mount_uploader :image, PostImageUploader 
 
   def update_slug
     self.slug = slug.parameterize
