395a05111cacc3ab3ad38417e24d140faf05cacd
pawelztef <pawelztef@gmail.com>
2017-09-18 07:30:07 +0100

    Basic structure incl. views forms and routes plus action implemented for query replies

--- app/controllers/backend/mailbox_controller.rb
+++ app/controllers/backend/mailbox_controller.rb
@@ -1,10 +1,12 @@
 class Backend::MailboxController < ApplicationController
   before_action :authenticate_admin!
   layout 'backend_layout'
+
   def index
     @queries = Query.all
     @title = 'Zapytania'
   end
+
   def show
     @query = Query.find(params[:id])
     @query.read = true
@@ -14,10 +16,16 @@ class Backend::MailboxController < ApplicationController
       format.js
     end
   end
+
+  def send_reply_mail
+    byebug
+    redirect_to backend_mailbox_index_path, notice: 'Twój email został wysłany.'
+  end
+
   def destroy
     @query = Query.find(params[:id])
     @query.destroy
-    redirect_to backend_mailbox_index_path, notice: 'Post was successfully destroyed.'
+    redirect_to backend_mailbox_index_path, notice: 'Zapytanie zostało usunięte z bazy danych.'
   end
 
 end
