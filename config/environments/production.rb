0c4b8f6836d644a038716e766fcddc5c5fc417cb
pawelztef <pawelztef@gmail.com>
2017-09-18 05:21:06 +0100

    changes made for production send mail configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,16 +1,23 @@
 Rails.application.configure do
   # config.action_mailer.delivery_method = :letter_opener_web
   # config.action_mailer.default_url_options = { host: 'localhost', port: 3000 } 
-  config.action_mailer.default_url_options = { host: '188.166.152.13'  } 
-  ActionMailer::Base.delivery_method = :smtp
-  ActionMailer::Base.smtp_settings = {
-    :address => "smtp.gmail.com",
-    :port => 587,
-    :user_name => Settings.smtp_username,
-    :password => Settings.smtp_pass,
-    :authentication => "plain",
-    :enable_starttls_auto => true
-  }
+# config.action_mailer.default_url_options = { host: 'polopazzo.pl'  } 
+config.action_mailer.delivery_method = :sendmail
+config.action_mailer.perform_deliveries = true
+config.action_mailer.raise_delivery_errors = true
+config.action_mailer.default_options = { from: 'contact@polopazzo.pl' }
+# config.action_mailer.perform_deliveries = true
+# config.action_mailer.perform_delivery_errors = true
+# config.action_mailer.default_options = { from: 'contact@polopazzo.pl' }
+#  ActionMailer::Base.delivery_method = :smtp
+#  ActionMailer::Base.smtp_settings = {
+#    :address => "smtp.gmail.com",
+#    :port => 587,
+#    :user_name => Settings.smtp_username,
+#    :password => Settings.smtp_pass,
+#    :authentication => "plain",
+#    :enable_starttls_auto => true
+#  }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
