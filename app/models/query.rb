5f98a50a716ff11dab0cc0ba3f4031dba731d64d
pawelztef <pawelztef@gmail.com>
2017-09-18 05:01:34 +0100

    Reply model generated

--- app/models/query.rb
+++ app/models/query.rb
@@ -1,4 +1,6 @@
 class Query < ActiveRecord::Base
+  has_many :replies, dependent: :delete_all
+
   # FULLNAME_REGEX = /\A[a-z,',-,ą,Ą,ę,Ę,ż,Ż,ć,Ć,ź,Ź,ó,Ó,ł,Ł]+(\s)[a-z,',-,ą,Ą,ę,Ę,ż,Ż,ć,Ć,ź,Ź,ó,Ó,ł,Ł]\z/
   FULLNAME_REGEX = /\A^[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.'-]+$\z/
   EMAIL_REGEX = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/
@@ -6,4 +8,5 @@ class Query < ActiveRecord::Base
   validates_format_of :fullname, with: FULLNAME_REGEX
   validates_format_of :email, with: EMAIL_REGEX
   validates :phone, numericality: { only_integer: true }
+  
 end
